a8a45fa5151f1d041907e1b040ae3391eac7b012
uddinbazmi67-lgtm <uddinbazmi67@gmail.com>
2025-10-29 16:41:16 +0530

    29/10/2025 third commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as AOS from 'aos';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  currentYear: number = new Date().getFullYear();
+export class AppComponent implements OnInit {
+    currentYear: number = new Date().getFullYear();
+  ngOnInit(): void {
+    AOS.init({
+      duration: 900, // animation speed
+      offset: 100,   // distance before triggering animation
+      once: true,    // only animate once
+      easing: 'ease-in-out'
+    });
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,3 +1,35 @@
 import { Component } from '@angular/core';
-@Component({selector:'app-home',templateUrl:'./home.component.html',styleUrls:['./home.component.css']})
-export class HomeComponent {}
+@Component({ selector: 'app-home', templateUrl: './home.component.html', styleUrls: ['./home.component.css'] })
+export class HomeComponent {
+    ngAfterViewInit() {
+        const roles = ["Angular Developer", "Full Stack Developer", "PHP Developer", "Team Lead"];
+        let index = 0;
+        const typingEl = document.getElementById("typing-text")!;
+
+        let charIndex = 0;
+        let isDeleting = false;
+
+        function typeEffect() {
+            const current = roles[index];
+            const visible = current.substring(0, charIndex);
+            typingEl.textContent = visible;
+
+            if (!isDeleting && charIndex < current.length) {
+                charIndex++;
+                setTimeout(typeEffect, 100);
+            } else if (isDeleting && charIndex > 0) {
+                charIndex--;
+                setTimeout(typeEffect, 50);
+            } else {
+                isDeleting = !isDeleting;
+                if (!isDeleting) {
+                    index = (index + 1) % roles.length;
+                }
+                setTimeout(typeEffect, 1000);
+            }
+        }
+
+        typeEffect();
+    }
+
+}
